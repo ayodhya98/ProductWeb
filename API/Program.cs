0b4ebafdf7d29f471c266ad7393cbaea019d7b5f
ayodhya98 <75997356+ayodhya98@users.noreply.github.com>
2023-10-26 15:12:44 +0530

    gg

--- API/Program.cs
+++ API/Program.cs
@@ -6,7 +6,7 @@ using DLL.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
